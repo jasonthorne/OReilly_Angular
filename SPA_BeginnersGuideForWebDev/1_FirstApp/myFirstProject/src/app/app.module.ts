18423de94f8b4dab8ae73a09d67db25925a1babf
jasonthorne <jasonthorne@live.ie>
2022-12-05 11:23:14 +0000

    changing starter component with bootstrap

--- SPA_BeginnersGuideForWebDev/1_FirstApp/myFirstProject/src/app/app.module.ts
+++ SPA_BeginnersGuideForWebDev/1_FirstApp/myFirstProject/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CustomerComponent]
 })
 export class AppModule { }
