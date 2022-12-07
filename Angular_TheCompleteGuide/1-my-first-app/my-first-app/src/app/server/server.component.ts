c6ea26ece4c07e794671202ff2294c5aa5820641
jasonthorne <jasonthorne@live.ie>
2022-12-07 13:37:02 +0000

    server component

---
+++ Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//component decorator (other types exist :P), to mark this class as a component.
+//needs passed an object with important meta data info: 
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent{
+
+}
