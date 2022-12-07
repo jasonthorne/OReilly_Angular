b2e5434f20c3651922d458a1b9671812733e98fa
jasonthorne <jasonthorne@live.ie>
2022-12-07 11:31:56 +0000

    bootstrap 3.4.1

--- Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/app.module.ts
+++ Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms' //import typecript forms module for use with ngmodel directive
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ///FormsModule //add forms module to imports 
   ],
   providers: [],
   bootstrap: [AppComponent]
