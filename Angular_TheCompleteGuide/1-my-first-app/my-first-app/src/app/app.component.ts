844a88600b1e350f2d26748aeae268e47de5c27f
jasonthorne <jasonthorne@live.ie>
2022-12-07 16:12:47 +0000

    component styles

--- Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/app.component.ts
+++ Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  //Inline styles (can have multiple styles in the array):
+  styles: [`
+  h3{
+    color: yellow;
+
+  }`]
 })
 
 
