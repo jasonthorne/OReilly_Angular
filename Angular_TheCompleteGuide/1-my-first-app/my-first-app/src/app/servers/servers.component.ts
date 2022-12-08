99503299d5acd47497763bbfba32ecc6feb665aa
jasonthorne <jasonthorne@live.ie>
2022-12-08 10:55:16 +0000

    understanding component selector

--- Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/servers/servers.component.ts
+++ Angular_TheCompleteGuide/1-my-first-app/my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+
+  //selector: 'app-servers',
+  //+++++++++ selector as an attribute isnstead of as an element ++++++++:
+  //selector: '[app-servers]',
+
+  //+++++++++ selector as a class instead of as an element: NOTE selecting by id WONT work in angular :P
+  selector: '.app-servers',
+
   //NOTE - Templates HAVE to be presnet here. Either external or internal
   /*templateUrl: './servers.component.html',*/ /* link to external html*/
   /* +++++++++++++++ intrnal html:  +++++++++++++ */
