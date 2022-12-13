461efc98524102ce4161b5ee9abc9c3a4e7426c3
jasonthorne <jasonthorne@live.ie>
2022-12-13 16:12:14 +0000

    eod

--- Angular_TheCompleteGuide/4-assignment3/assignment3/src/app/app.component.ts
+++ Angular_TheCompleteGuide/4-assignment3/assignment3/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment3';
+
+  canShowMsg: boolean = false;
+
+  showMsg(){
+    this.canShowMsg = true;
+  }
+
+
+
 }
