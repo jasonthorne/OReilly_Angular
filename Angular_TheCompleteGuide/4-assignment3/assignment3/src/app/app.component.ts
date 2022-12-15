cf5279dc905d9c46ca45d2d38439fff707633ad5
jasonthorne <jasonthorne@live.ie>
2022-12-15 16:22:52 +0000

    eod

--- Angular_TheCompleteGuide/4-assignment3/assignment3/src/app/app.component.ts
+++ Angular_TheCompleteGuide/4-assignment3/assignment3/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Time } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `.online{
+        color: white;
+    }`
+  ] 
 })
+
 export class AppComponent {
   title = 'assignment3';
 
   canShowMsg: boolean = false;
-  times: Array<string> = []; //listen ot if this is at size of 5 +++++++++++++
+  times: Array<string> = []; //listen ot if this is at size of 5 +++++++++++
+  canChangeStyles = false;
   
 
   toggleMsg(){
@@ -21,12 +27,27 @@ export class AppComponent {
     //add date string to dates:
     this.times.push(new Date(Date.now()).toLocaleTimeString());
 
+    //change styles when hit length 5:
+    if(this.times.length == 5){
+      this.canChangeStyles = true;
+    }
+
+
     console.log(this.times);
+
+    if(this.canChangeStyles){
+      console.log("change styles");
+    }
 
 
     
   }
 
+
+  getColour(){
+    //return green or red deepending on server status:
+    return this.canChangeStyles === false ? 'transparent': 'blue';
+  }
 
 
 }
