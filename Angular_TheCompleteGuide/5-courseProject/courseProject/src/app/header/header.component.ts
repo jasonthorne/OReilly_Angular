515587fa54eda901a2c7735e8072173dd6702a95
jasonthorne <jasonthorne@live.ie>
2022-12-20 14:48:01 +0000

    recipies component

--- Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/header/header.component.ts
+++ Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/header/header.component.ts
@@ -1,3 +1,12 @@
+import {Component} from '@angular/core';
+
+//pass js objec to decorator to configure this component:
+@Component({
+    selector: "app-header", //selector allows us to use this commponent
+    templateUrl: "./header.component.html" //points to html file for this component
+})
+
+
 export class HeaderComponent{
-    
+
 }
