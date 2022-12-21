68f680014b8976bb460b7349c18bf855935f1ea7
jasonthorne <jasonthorne@live.ie>
2022-12-21 13:21:01 +0000

    eod

--- Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'; //++++++++++++ Import recipe model
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [ //recipes is an array of recipe objects
+    new Recipe("a", "b", "c")
+  ]; 
 
 }
