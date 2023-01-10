f21f45acc0f3e14a879fc4b25f999846ea411e42
jasonthorne <jasonthorne@live.ie>
2023-01-10 13:51:28 +0000

    adding content to recipes comonents

--- Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,8 +7,7 @@ import { Recipe } from '../recipe.model'; //++++++++++++ Import recipe model
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [ //recipes is an array of recipe objects
-    new Recipe("a", "b", "c")
-  ]; 
+  recipes: Recipe[] = [ //recipes is an array of Recipe (model) objects ++++++++
+    new Recipe("a", "b", "https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg")];
 
 }
