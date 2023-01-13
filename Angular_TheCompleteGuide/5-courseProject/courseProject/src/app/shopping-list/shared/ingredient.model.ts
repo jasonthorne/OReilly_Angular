7ab066d5d342b2830506cd5cfec9a9c25031353b
jasonthorne <jasonthorne@live.ie>
2023-01-13 13:28:04 +0000

    create ingredients model

--- Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/shopping-list/shared/ingredient.model.ts
+++ Angular_TheCompleteGuide/5-courseProject/courseProject/src/app/shopping-list/shared/ingredient.model.ts
@@ -1,11 +1,18 @@
 //model for defining ingredient:
 
 export class Ingredient {
+    /*
     public name: string;
     public amount: number;
 
     constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    }*/
 
-    }
+    //++++++++++ SAME AS ABOVE :) (so 2 public properties are auto created and assigned vals from constructor params)++++++
+    constructor(public name:string, public amount:number){}
+
+
 
 }
